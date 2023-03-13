9de96710ce70e6dfdd0e46600d861c90214fce05
mishalp7 <82849256+mishalp7@users.noreply.github.com>
2023-03-13 18:39:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 # allows us to access the random function
 import random
 from time import sleep
